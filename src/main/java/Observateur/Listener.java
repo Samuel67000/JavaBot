00a664357e911c95e8e2842c3865dfabf1ff7701
LADWEIN SAMUEL <samuel.ladwein@etu.unistra.fr>
2020-12-16 16:48:02 +0100

    Delete reaction + Delete duplicate reaction

--- src/main/java/Observateur/Listener.java
+++ src/main/java/Observateur/Listener.java
@@ -5,6 +5,7 @@ import Controller.EventManager;
 import Controller.JDAManager;
 import Vues.CommandView;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
@@ -33,12 +34,20 @@ public class Listener extends ListenerAdapter {
         String tag = message.getEmbeds().get(0).getFooter().getText().split(":")[1];
         if(tag.equalsIgnoreCase("config")){
             if(event.getUser() != ControllerMetier.proprietaire){
-                System.out.println(event.getReaction().getReactionEmote().getId());
-                message.removeReaction(event.getReaction().getReactionEmote().getId()).queue();
+                message.removeReaction(event.getReaction().getReactionEmote().getAsReactionCode(), event.getUser()).queue();
+            }
+            else{
+
+                for(MessageReaction react:message.getReactions()){
+                    if(react.getCount() > 1 && !react.toString().equals(event.getReaction().toString())){
+                        System.out.println(event.getReaction().toString());
+                        System.out.println(react.toString());
+                        message.removeReaction(react.getReactionEmote().getAsReactionCode(),ControllerMetier.proprietaire).queue();
+                    }
+                }
             }
         }
-        //random
-        System.out.println(message.getEmbeds().get(0).getTitle());
+
 
 
     }
